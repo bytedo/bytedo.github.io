bccba12817f5625bd3790052a6654d5d2896026e
宇天 <yutent.io@gmail.com>
2021-01-29 18:08:11 +0800

    更新主页

---
+++ wcui/assets/js/doc.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @author yutent<yutent.io@gmail.com>
+ * @date 2021/01/29 14:12:03
+ */
+
+import '//unpkg.yutent.top/anot/dist/anot.js'
+import '//unpkg-dev.yutent.top/@bytedo/wcui/dist/form/button.js'
+import '//unpkg-dev.yutent.top/@bytedo/wcui/dist/code/index.js'
+
+Anot({
+  $id: 'doc',
+  state: {},
+  mounted() {},
+  methods: {}
+})
